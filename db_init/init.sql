8aacc2101a9e85b6f5075295d8ab1d489c0ee771
Jesse-Cachai <155775522+Jesse-Cachai@users.noreply.github.com>
2024-12-30 21:16:00 -0600

    Update init.sql
    Added the correct table schema to the init.sql

--- db_init/init.sql
+++ db_init/init.sql
@@ -1,8 +1,30 @@
--- Create the surveys table if it doesn't already exist
+-- Create the surveys table
 CREATE TABLE IF NOT EXISTS surveys (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    feedback TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    rd_name TEXT NOT NULL,
+    date_seen DATE NOT NULL,
+    mrn_patient TEXT NOT NULL,
+    reason_for_visit TEXT NOT NULL,
+    ina_reason TEXT,
+    ra_reason TEXT,
+    outcome_risk TEXT,
+    ina_timeliness TEXT,
+    ina_late_reason TEXT,
+    ina_reason_other TEXT,
+    ina_days_late TEXT,
+    ra_timeliness TEXT,
+    ra_late_reason TEXT,
+    ra_reason_other TEXT,
+    ra_days_late TEXT,
+    consult_reason TEXT,
+    consult_timeliness TEXT,
+    consult_late_reason TEXT,
+    consult_reason_other TEXT,
+    consult_days_late TEXT,
+    education TEXT,
+    nutrition_support TEXT,
+    malnutrition TEXT,
+    additional_info_optional TEXT,
+    patient_care_time TEXT NOT NULL,
+    formal_meeting_time TEXT
 );
